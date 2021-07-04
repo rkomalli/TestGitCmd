508e64ea14a184f78767e93414aee29801f02840
rkomalli <komalliramesh@gmail.com>
2021-07-04 16:30:27 +0530

    mul

--- newfile.py
+++ newfile.py
@@ -6,3 +6,6 @@ print 'The addition of a and b is: ',c
 #perform subtraction of a and b
 d = a-b
 print 'The addition of a and b is: ',c
+#perform multiplication of a and b
+e = a*b
+print 'The multiplication of a and b is: ',c
