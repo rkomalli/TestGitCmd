d110e7cd223e9644b8107afd38a2241f67509fe9
rkomalli <komalliramesh@gmail.com>
2021-07-04 18:54:29 +0530

    test mul b 50

--- testfile.py
+++ testfile.py
@@ -1,8 +1,11 @@
 a = 10
-b = 0.000
+b = 50
 #perform addition of a and b
 c = a+b
 print 'The addition of a and b is: ',c
 #perform subtraction of a and b
 d = a-b
 print 'The addition of a and b is: ',d
+#perform multipplication of a and b
+e = a*b
+print 'The addition of a and b is: ',e
