0b08d89553f8d0cde36546ed7614d58b9eb394ae
rkomalli <komalliramesh@gmail.com>
2021-07-04 17:50:32 +0530

    test-div b 40

--- testfile.py
+++ testfile.py
@@ -1,8 +1,14 @@
 a = 10
-b = 0.000
+b = 40
 #perform addition of a and b
 c = a+b
 print 'The addition of a and b is: ',c
 #perform subtraction of a and b
 d = a-b
 print 'The addition of a and b is: ',d
+#perform multiplication of a and b
+e = a*b
+print 'The multiplication of a and b is: ',e
+#perform division of a and b
+e = a/b
+print 'The division of a and b is: ',e
